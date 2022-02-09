ac8e3ccd2fe4ab160105d5f4c89001e9ed33d6e8
Alex Oliveira <alexskntks@outlook.com>
2022-02-09 14:23:45 -0300

    testes concluídos

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
 # Back-end-nodejs-ignite-01
 Desafio 01 da trilha de nodejs do curso ignite da Rocketseat.
+
+Essa será uma aplicação para gerenciar tarefas (em inglês *todos*). Será permitida a criação de um usuário com `name` e `username`, bem como fazer o CRUD de *todos*:
+
+- Criar um novo *todo*;
+- Listar todos os *todos*;
+- Alterar o `title` e `deadline` de um *todo* existente;
+- Marcar um *todo* como feito;
+- Excluir um *todo*;
+
+Tudo isso para cada usuário em específico (o `username` será passado pelo header).
+
+## Template da aplicação
+
+[Link](https://github.com/rocketseat-education/ignite-template-conceitos-do-nodejs)
+
+Após clonar o repositório, instale todas as dependências com
+
+    npm install
+
+## Requisições da aplicação
+
+- POST `/users`
+
+    A rota deve receber `name`, e `username` dentro do corpo da requisição.
+
+        { 
+            name: "Fulano"
+            username: "FulanTrex"
+        }
+
+- GET `/todos`
+
+    A rota recebe, pelo header da requisição, uma propriedade `username` contendo o username do usuário e retorna uma lista com todas as tarefas desse usuário.
+
+- POST `/todos`
+
+    A rota deve receber `title` e `deadline` dentro do corpo da requisição e, uma propriedade `username` contendo o username do usuário dentro do header da requisição. Exemplo de body:
+
+        { 
+            title: "Ir a biblioteca"
+            username: "2020-02-21"
+        }
+
+    O _todo_ tem a estrutura:
+
+        { 
+            id: 'uuid', // precisa ser um uuid
+            title: 'Nome da tarefa',
+            done: false, 
+            deadline: '2021-02-27T00:00:00.000Z', 
+            created_at: '2021-02-22T00:00:00.000Z'
+        }
+
+- PUT `/todos/:id`
+
+    A rota deve receber, pelo header da requisição, uma propriedade `username` contendo o username do usuário e receber as propriedades `title` e `deadline` dentro do corpo, para editar o *todo* do usuário.
+
+- PATCH `/todos/:id/done`
+
+    A rota recebe, pelo header da requisição, uma propriedade `username` contendo o username do usuário e altera a propriedade `done` para `true` no *todo* que possuir um `id` igual ao `id` presente nos parâmetros da rota. O id é um _uuid_. Em outras palavras, marca um *todo* como concluído.
+
+- DELETE `/todos/:id`
+
+    A rota recebe, pelo header da requisição, uma propriedade `username` contendo o username do usuário e excluir o *todo* que possuir um `id` igual ao `id` presente nos parâmetros da rota.
+
+## Regras
+
+1. Não deve ser possível criar um usuário com username já existente.
+2. Não deve ser possível editar/apagar/marcar como concluído um *todo* com id que não existe.
+3. Não deve ser possível buscar a lista de *todos* de um usuário não existente.
+4. Não deve ser possível criar um *todo* para um usuário que não existe
+
+## Testes
+
+Você pode testar cada requisição fazendo uso do `insomnia` ou rodar o comando:
+
+    npm tests
--- src/index.js
+++ src/index.js
@@ -1,41 +1,146 @@
 const express = require('express');
-const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
-app.use(cors());
+const { v4:uuidv4 } = require("uuid");
+
+const cors = require('cors');
+const res = require('express/lib/response');
+const req = require('express/lib/request');
+
 app.use(express.json());
+app.use(cors());
 
-// const users = [];
+// O user é passado pelo header em TODAS as requisições. Ele é identificado pelo username
+const users = [];
 
+app.get("/", (req, res)=>{
+  return res.json({nome: "Alex Oliveira"});
+})
+
+//Verifica se o user passado pelo header já existe
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if(!user){
+    return response.status(404).json({error: "User not found!"});
+  }
+  request.user = user;
+  return next();
 }
 
-app.post('/users', (request, response) => {
-  // Complete aqui
+//Verifica se o user passado no request.body já existe
+function checkIfAlreadyExistsUsername(request, response, next) {
+  const { username } = request.body;
+
+  const user = users.find((user) => user.username === username);
+
+  if(user){
+    return response.status(400).json({error: "Username already exists!"});
+  }
+
+  request.user = user;
+  return next();
+}
+
+//Verifica se o todo com id passado no request.params existe
+//Coloca o id e o todo na requisição
+function checksExistsUserTodo(request, response, next){
+  const { id } = request.params;
+
+  const { user } = request;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if(!todo){
+    return response.status(404).json({error: "Todo id not found!"});
+  }
+
+  request.id = id;
+  request.todo = todo;
+
+  return next();
+}
+
+
+// Cria um novo user com body na requisição
+app.post('/users', checkIfAlreadyExistsUsername, (request, response) => {
+  const { name, username } = request.body;
+
+  const user = {
+    id: uuidv4(),
+    name: name,
+    username: username,
+    todos: []
+  }
+
+  users.push(user);
+
+  return response.status(201).json(user);
 });
 
+//Retorna todos os todos do user
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  return response.json(user.todos);
 });
 
+//Cria um todo para o usuário
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  const { title, deadline } = request.body;
+
+  const todo = {
+    id: uuidv4(),
+    title: title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+
+//Faz alteração do título e data limite do todo, com id passado pelo request.params
+app.put('/todos/:id', checksExistsUserAccount, checksExistsUserTodo, (request, response) => {
+  const { todo } = request;
+
+  const { title, deadline } = request.body;
+
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.status(200).json(todo);
+
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+//Marca um todo como feito
+app.patch('/todos/:id/done', checksExistsUserAccount, checksExistsUserTodo, (request, response) => {
+  const { todo } = request;
+
+  todo.done = true;
+
+  return response.status(200).json(todo);
 });
 
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+
+//Apaga um todo
+app.delete('/todos/:id', checksExistsUserAccount, checksExistsUserTodo, (request, response) => {
+  const { user, id } = request;
+
+  //Filter retorna um array com todos os elementos que passaram no  teste.
+  //Ou seja, aqueles cujo id é diferente do id que vamos remover
+  //E atualizamos o array de todos do usuário
+  user.todos = user.todos.filter((todo)=> todo.id != id);
+
+  return response.status(204).send();
 });
 
 module.exports = app;
